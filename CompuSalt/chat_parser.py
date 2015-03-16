7b33403d3bade18287b0b627dc405d151740f934
Eric <emstin1@gmail.com>
2015-03-16 07:59:09 -0400

    added docstrings and formatted readme

--- CompuSalt/chat_parser.py
+++ CompuSalt/chat_parser.py
@@ -6,12 +6,16 @@ win = re.compile('wins!')
 
 #stripping the cruft from our chat string 
 def strip(chat):
+    '''strips out the IRC info from the message'''
     match = re.search(regex.pattern, chat)
     if match:
         msg = chat[match.end():]
         return msg
 #if our chat string/message is announcing a match or declaring a winner, strip the cruft and return only the important bits        
 def win_open(message):
+    '''this will decide whether the message is announcing a winner or the start of
+        a new match and strip out the relevant info as necessary
+    '''
     openner = re.search(open.pattern, message)
     winner = re.search(win.pattern, message)
     tier = re.search('Tier', message)
@@ -24,6 +28,7 @@ def win_open(message):
 
 #returns a list with our two fighters
 def fighters(message):
+    '''getting the individual fighters from a match announcnement and returning them as a list'''
     vs = message.find('vs')
     exclaim = message.find('!')
     
@@ -34,8 +39,10 @@ def fighters(message):
 
 #returns the winner of the fight
 def winner(message):
+    '''getting the winner from a match end announcement'''
     return message[:message.find('wins')-1]
 
 #returns the tier of the current match
 def tier_strip(message):
+    '''getting the tier from the match announcement'''
     return message[message.find('Tier')-2:message.find('Tier')-1]
--- CompuSalt/stat_tracker.py
+++ CompuSalt/stat_tracker.py
@@ -1,12 +1,21 @@
 import chat_parser as chat
 
 def get_stats(fighter, tier, stats):
+    '''
+        taking the a fighter from the list returned by the fighters function
+        and retrieving the stats from the json file (if they exist) or creating
+        the stats (if they do not exist).
+    '''
     if fighter in stats[tier]:
         return stats[tier][fighter]
     else:
         return {'wins': 0, 'loses': 0}
     
 def winner_loser(fighter1, fighter2, stats1, stats2, winner):
+    '''
+        takes two fighters, and the stats from them (retrived with get_Stats)
+        and the winner from the winner function and adjusts the stats accordingly.
+    '''
     if winner == fighter1:
         stats1['wins'] += 1
         stats2['loses'] += 1
